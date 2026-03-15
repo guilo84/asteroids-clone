7ac0f0a0020ab1591d4e343cb4d98d405679455b
guilo84 <mathew.tolbert@gmail.com>
2026-03-15 14:08:30 -0400

    A: added screen to main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")      
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create a clock object
+    clock = pygame.time.Clock()
+
+    #create infiite game loop
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
